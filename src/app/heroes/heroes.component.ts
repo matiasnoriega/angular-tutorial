008a2fa6bd7d15398984e269f84d30c81b764f68
Matias Nicolas Noriega <matiasnoriega86@gmail.com>
2019-05-24 15:56:41 -0300

    implementacion de servicios en HeroesComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
     selector: 'app-heroes',
@@ -10,15 +10,21 @@ import { HEROES } from '../mock-heroes';
 
 export class HeroesComponent implements OnInit {
 
-    heroes = HEROES;
+    heroes: Hero[];
     selectedHero: Hero;
 
+    constructor(private heroService: HeroService) { }
+    
     onSelect(hero: Hero): void{
         this.selectedHero = hero;
     }
-    constructor() { }
+
+    getHeroes(): void{
+        this.heroes = this.heroService.getHeroes();
+    }
 
     ngOnInit() {
+        this.getHeroes();
     }
 
 }
