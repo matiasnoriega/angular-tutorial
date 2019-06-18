2d3f73da4b6877eb0bd3068a12709a976b93fee1
Matias Nicolas Noriega <matiasnoriega86@gmail.com>
2019-06-18 10:50:25 -0300

    getHero con tap y handleError

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -27,8 +27,12 @@ export class HeroService {
   }
 
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroeService: Fetched hero id=${id}!`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url)
+    .pipe(
+      tap(_ => this.log(`Fetched hero ID: ${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    )
   }
 
   private log(message: string){
